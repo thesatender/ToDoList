7772ab0945640fcc0c4729c9bc11793dd2103b44
Satender sharma <satender80010336@gmail.com>
2023-06-06 14:06:51 +0530

    ToDoList version 1.0.0 complete

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs")
+
+let items = ["Buy food", "Cook food", "Eat food"];
+
+app.get("/", function(req, res) {
+    
+    let today = new Date();
+    let currentDay = today.getDay();
+    
+    let options = {
+        weekday: "long", 
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res) {
+    items.push(req.body.newItem)
+    res.redirect("/");
+});
+
+app.listen(3000, function() {
+    console.log("Server is running at port 3000.");
+})
